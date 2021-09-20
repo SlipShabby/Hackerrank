a9ff837f3cdc5cecf11c37defdbcc105aacdc053
SlipShabby <sd.djaan@gmail.com>
2021-09-20 11:26:55 -0700

    solutions

--- Python/strings.py
+++ Python/strings.py
@@ -34,5 +34,43 @@ if __name__ == '__main__':
 for check in l:
     print(any(check(c) for c in s))
 
+# text alignment
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+
+# text wrap
+def wrap(string, w):
+    s = ""
+    for i in range (0,len(string),w):
+            s += string[i:i+w] + '\n'
+    return s
+
+
+
+
+
 
 
