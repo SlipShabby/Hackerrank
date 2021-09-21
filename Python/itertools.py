e61f7589337b2668c4894557f48393d906725245
SlipShabby <sd.djaan@gmail.com>
2021-09-21 14:29:10 -0700

    solutions

--- Python/itertools.py
+++ Python/itertools.py
@@ -18,3 +18,57 @@ s = list(sorted(permutations(a,int(b))))
 #     print(''.join(s[i]))
 
 print(*[''.join(i) for i in s], sep = '\n')
+
+# combinations
+
+from itertools import combinations
+
+a,b = input().split()
+# s = list(combinations(sorted(a), int(i)))
+for i in range(1,int(b)+1):
+    print(*[''.join(i) for i in combinations(sorted(a), int(i))], sep= '\n')
+
+
+# combinations with replacemnet
+
+from itertools import combinations_with_replacement
+a,b = input().split()
+
+s = list(combinations_with_replacement(sorted(a), int(b)))
+
+print(*[''.join(i) for i in (s)], sep='\n')
+
+# compress the string
+
+from itertools import groupby
+a = [(len(list(g)), int(k)) for k, g in groupby(input())]
+print(*a)
+
+# iterables & iterators
+
+from itertools import combinations
+
+n = int(input())
+a = input().split()
+k = int(input())
+
+c = list(combinations(a,k))
+f = [i for i in c if 'a' in i]
+print(len(f)/len(c))
+
+# maximaze it
+
+from itertools import product
+
+k , m = map(int, input().split())
+l = [(i**2 for i in list(map(int, input().split()))[1:]) for _ in range(k)]
+# l2 = [sum(i)%m for i in range(list(*product))]
+l2 = []
+for i in list(product(*l)):
+    l2.append(sum(i)%m)
+print(max(l2))
+
+
+
+
+
