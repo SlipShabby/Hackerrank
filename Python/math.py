860e1c1b707ffa5a056f5260b4239d739b5ecf67
SlipShabby <sd.djaan@gmail.com>
2021-09-21 14:05:01 -0700

    solutions

--- Python/math.py
+++ Python/math.py
@@ -13,3 +13,26 @@ d ='\u00b0'
 
 print(round(math.degrees(math.acos(bc/math.hypot(ab,bc)))),d, sep = '')
 
+# triangle quest 
+
+for i in range(1,int(input())):
+    print((10**(i)//9)*i)
+
+# triangle quest 2
+
+set(map(lambda x:print((10**x//9)**2),range(1,int(input())+1)))
+
+# mod divmode
+
+a, b = [int(input()) for i in range(2)]
+print(a//b, a%b, a.__divmod__(b), sep='\n')
+
+# mod power
+
+a,b,c = [int(input()) for i in range(3)]
+print(pow(a,b), pow(a,b,c), sep = '\n')
+
+# integers come in all size
+
+a,b,c,d = [int(input()) for i in range(4)]
+print(pow(a,b) + pow(c,d))
