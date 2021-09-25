a78d78bc0968ee1e752d4de7b7772792080cbd24
SlipShabby <sd.djaan@gmail.com>
2021-09-25 15:46:58 -0700

    solutions

---
+++ Python/functionals.py
@@ -0,0 +1,13 @@
+# map and lambda
+
+cube = lambda x: x**3 # complete the lambda function 
+
+def fibonacci(n):
+    a,b = 0,1
+    for i in range(n):
+        yield a
+        a,b = b,a+b
+
+        
+    # return a list of fibonacci numbers
+
