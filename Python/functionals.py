ced3e01aee26ca0a8fdd11c5d4dead478fae50d7
SlipShabby <sd.djaan@gmail.com>
2021-09-25 15:48:16 -0700

    solutions

--- Python/functionals.py
+++ Python/functionals.py
@@ -11,3 +11,39 @@ def fibonacci(n):
         
     # return a list of fibonacci numbers
 
+# reduce 
+
+def product(fracs):
+    t = reduce((lambda x,y: x*y) ,fracs)# complete this line with a reduce statement
+    return t.numerator, t.denominator
+
+
+# validate email with filter
+
+import re
+
+def fun(email):
+    # try:
+    #     username, mail = email.split("@")
+    #     website, domain = mail.split(".")
+    # except ValueError:
+    #     return False
+    
+    # if not username.replace("-", "").replace("_", "").isalnum():
+    #     return False
+    # if not website.isalnum():
+    #     return False
+    # if len(domain) > 3:
+    #     return False
+    # return True
+
+    regex = r"^[A-Za-z0-9_-]+@[A-Za-z0-9]+\.[a-zA-Z]{0,3}$"     
+
+    if not re.match(regex, email):
+        return False
+    return True
+    
+  
+
+
+
