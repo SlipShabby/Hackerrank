c23f8cad216d1215e514c7616ebce0d1111ccf42
SlipShabby <sd.djaan@gmail.com>
2021-09-25 15:43:42 -0700

    solutions

--- Python/built_ins.py
+++ Python/built_ins.py
@@ -5,3 +5,9 @@ x,y = map(int, input().split())
 L = [map(float, input().split()) for i in range(y)]
 [print(sum(i)/y) for i in zip(*L)]
 
+# input()
+x,k=map(int, input().split())
+print (k==eval(input()))
+
+# eval
+eval(input())
