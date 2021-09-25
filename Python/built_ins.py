db84976bab215dd9c3a25627be27a28549bf9d9e
SlipShabby <sd.djaan@gmail.com>
2021-09-25 15:45:59 -0700

    solutions

--- Python/built_ins.py
+++ Python/built_ins.py
@@ -20,3 +20,28 @@ print(all(int(i)>=0 for i in l) and any(i == i[::-1]for i in l))
 
 s =sorted(input(),key=lambda x: (x.isdigit(),x.isdigit() and int(x)%2==0, x.isupper(), x.islower(),x))
 print(*s, sep='')
+
+# athlete sort
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+
+
+arr.sort(key=lambda x: x[k])
+[print(*i, sep=' ') for i in arr]
+
+
