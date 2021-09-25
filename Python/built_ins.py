727f74ba1546eeb66d9b7e0ff1b804b37e1fcc41
SlipShabby <sd.djaan@gmail.com>
2021-09-25 15:44:59 -0700

    solutions

--- Python/built_ins.py
+++ Python/built_ins.py
@@ -11,3 +11,12 @@ print (k==eval(input()))
 
 # eval
 eval(input())
+
+# any and all
+num, l = input(), input().split(' ')
+print(all(int(i)>=0 for i in l) and any(i == i[::-1]for i in l))
+
+# ginortS
+
+s =sorted(input(),key=lambda x: (x.isdigit(),x.isdigit() and int(x)%2==0, x.isupper(), x.islower(),x))
+print(*s, sep='')
