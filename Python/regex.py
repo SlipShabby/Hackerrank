269ceb8566753f03e8a20dfdce31fe3e40ca0165
SlipShabby <sd.djaan@gmail.com>
2021-09-26 23:06:00 -0700

    solutions

--- Python/regex.py
+++ Python/regex.py
@@ -1,3 +1,6 @@
+import re
+
+
 # detect float num
 
 # for i in range(int(input())):
@@ -10,8 +13,7 @@
 #             print('True')
 #     except:        
 #         print('False')
-    
-import re
+
 
 for i in range(int(input())):
     print(bool(re.match(r'^[-+]?[0-9]*\.[0-9]+$', input())))
@@ -38,3 +40,22 @@ f = re.findall(regex, s, re.I)
 
 print('\n'.join(f or['-1']))
 
+# re.start() & re.end()
+
+s, p = input(), input()
+
+# if p in s:
+#     print(*[(i.start(), (i.start()+len(p)-1)) for i in re.finditer(r'(?={})'.format(p), s)], sep='\n')
+# else:
+#     print('(-1, -1)')
+
+matches = list(re.finditer(r'(?={})'.format(p), s))
+if matches:
+    print('\n'.join(str((match.start(),
+          match.start() + len(p) - 1)) for match in matches))
+else:
+    print('(-1, -1)')
+
+# Validating Roman Numerals
+
+regex_pattern = r"^M{0,3}(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})$"
