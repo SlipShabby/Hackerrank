fd791baefedbcf1076cbba3c53cdbe0d5dd1c125
SlipShabby <sd.djaan@gmail.com>
2021-09-27 14:48:12 -0700

    regex solutions

--- Python/regex.py
+++ Python/regex.py
@@ -81,3 +81,79 @@ for i in range(int(input())):
     if validEmail:
         print(email.utils.formataddr((name, mail)))
 
+
+# hex color code
+
+# pattern=r'(#[0-9a-fA-F]{3,6}){1,2}[^\n ]'
+pattern = r'(?<!^)(#(?:[\da-f]{3}){1,2})'
+for i in range(int(input())):
+    for x in re.findall(pattern,input(), re.I):
+        print(x)
+
+# HTML Parser 1
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+        for ele in attrs:
+            print ('->',ele[0],'>',ele[1])
+
+# instantiate the parser and fed it some HTML
+
+            
+parser = MyHTMLParser()
+parser.feed(''.join([input().strip() for _ in range(int(input()))]))
+
+
+# HTML Parser 2
+
+from html.parser import HTMLParser
+
+# class MyHTMLParser(HTMLParser):
+#     def handle_comment(self, data):
+#         n = len(data.split('\n'))
+#         if n > 1:
+#             print(">>> Multi-line Comment")
+#         else:
+#             print(">>> Single-line Comment")
+#         if data.strip():
+#             print(data)
+          
+#     def handle_data(self, data):
+#         if data.strip():
+#             print(">>> Data")
+#             print(data)
+  
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' in data:
+            comment = 'Multi'
+        else:
+            comment = 'Single'
+        print(f'>>> {comment}-line Comment\n{data.strip()}')
+  
+    def handle_data(self, data):
+        if data != '\n':
+            print(f'>>> Data\n{data}')
+
+  
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
+
