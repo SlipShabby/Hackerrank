5ef60b676fdbeb50db254bfd51cfdb14d131b8ec
SlipShabby <sd.djaan@gmail.com>
2021-10-01 01:25:18 -0700

    upd

--- Python/decorators.py
+++ Python/decorators.py
@@ -4,3 +4,13 @@ def wrapper(f):
     def fun(l):
         f('+91 {} {}'.format(n[-10:-5], n[-5:]) for n in l)
     return fun
+
+
+# name directory
+
+def person_lister(f):
+    def inner(people):
+        return map(f, sorted(people, key = lambda x: int(x[2])))
+        # complete the function
+    return inner
+
