98d221357a5311228a2a0793ad4daa39204c8772
SlipShabby <sd.djaan@gmail.com>
2021-10-01 01:23:15 -0700

    upd

--- Python/xml.py
+++ Python/xml.py
@@ -2,6 +2,20 @@
 
 import xml.etree.ElementTree as etree
 
-
 def get_attr_number(node):
     return etree.tostring(node).count(b'=')# your code goes here
+
+# find max depth
+
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    level += 1
+    if level >= maxdepth:
+        maxdepth = level
+    for child in elem:
+        depth(child, level)
+        
+   
+ 
+
