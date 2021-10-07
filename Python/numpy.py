f1f39d255c81442721966896d88ae6ef975ceb80
SlipShabby <sd.djaan@gmail.com>
2021-10-07 13:27:00 -0700

    upd

--- Python/numpy.py
+++ Python/numpy.py
@@ -1,6 +1,14 @@
+import numpy
+
 # arrays
 
 def arrays(arr):
     a = numpy.array(arr[::-1], float)
     return a
 
+# reshape
+
+
+
+arr = numpy.array(list(map(int, input().split())))
+print(numpy.reshape(arr, (3,3)))
