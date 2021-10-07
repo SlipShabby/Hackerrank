70fd1c1cf467d00d3c8060ad216ce12a944c2798
SlipShabby <sd.djaan@gmail.com>
2021-10-07 13:32:18 -0700

    upd

--- Python/numpy.py
+++ Python/numpy.py
@@ -8,7 +8,14 @@ def arrays(arr):
 
 # reshape
 
-
-
 arr = numpy.array(list(map(int, input().split())))
 print(numpy.reshape(arr, (3,3)))
+
+# transpose and flatten
+
+n, m= list(map(int, input().split()))
+l = numpy.array([input().strip().split() for i in range(n)], int)
+arr = numpy.transpose(l)
+print(arr)
+print(l.flatten())
+
