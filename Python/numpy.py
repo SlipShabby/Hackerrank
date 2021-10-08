36a961d397f6c0fc99d7c2f35f31db3e591c400b
SlipShabby <sd.djaan@gmail.com>
2021-10-08 10:48:07 -0700

    upd

--- Python/numpy.py
+++ Python/numpy.py
@@ -42,4 +42,10 @@ n , m = map(int,input().split())
 arr = numpy.eye(n, m, k = 0)
 print(arr)
 
+# array mathematics
 
+n, m = map(int, input().split())
+a, b = (numpy.array([input().split() for _ in range(n)], dtype=int) for _ in range(2))
+print(a+b, a-b, a*b, a//b, a%b, a**b, sep = '\n')
+
+# 
